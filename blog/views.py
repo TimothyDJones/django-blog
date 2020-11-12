fc624f0e838524d21bd582f767a8bf9848b6af52
Tim Jones <tdjones74021@yahoo.com>
2020-11-12 13:21:23 -0600

    Add database migrations and update templates to display individual posts.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
 def home(request):
-    return render(request, "home.html")
+    
+    posts = Post.objects.all()
+
+    return render(request, "home.html", 
+        {"section": "home",
+         "posts": posts,
+        })
